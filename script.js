cd779bd3721d86eea3d738b4d71cb722a528fc76
trezor13 <treasureharden14@gmail.com>
2022-04-14 10:27:49 -0700

    added new Javascript codes for responsiveness

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+// author : tresor
+
+const toggleButton = document.getElementsByClassName('navbar-toggler')[0]
+const navbarNav = document.getElementsByClassName('navbar-nav')[0];
+const navItem1=document.getElementsByClassName('nav-item')[0];
+const navItem2=document.getElementsByClassName('nav-item')[1];
+const navItem3=document.getElementsByClassName('nav-item')[2];
+const navItem4=document.getElementsByClassName('nav-item')[3];
+const navItem5=document.getElementsByClassName('nav-item')[4];
+const navItem6=document.getElementsByClassName('nav-item')[5];
 // for toggle button 
 toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active');
+    navbarNav.classList.toggle('active');
+    navItem1.classList.toggle("active")
+    navItem2.classList.toggle("active")
+    navItem3.classList.toggle("active")
+    navItem4.classList.toggle("active")
+    navItem5.classList.toggle("active")
+    navItem6.classList.toggle("active")
 })
+// author : tresor
 
 
 // for Sliders 
@@ -25,6 +41,16 @@ buttons.forEach(button => {
     delete activeSlide.dataset.active
   })
 })
+
+// author :tresor
+//show/hide nav links
+
+const showNavBarbutton=document.getElementsByClassName("navbar-toggler")[0]
+console.log(showNavBarbutton);
+
+//show/hide nav links
+
+// author :tresor
 
 
 
